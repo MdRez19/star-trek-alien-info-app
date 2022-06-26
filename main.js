612116093171d1643bbe308c66740f5454f978db
rezaul choudhury <rofy2009@gmail.com>
2022-06-27 00:49:13 +0100

    html,js,style been added!

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-async function apiRequest() {
+console.log('hello')
 
+document.querySelector('#getButton').addEventListener('click', apiRequest)
+
+async function apiRequest() {
+    const alienName = document.querySelector('input').value
+    try {
+        const response = await fetch(`https://star-trek-api-demo-test.herokuapp.com/api/${alienName}`)
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
 }
